f2ee2cf765ca0b0f1405f9bf50de584bfc95dc9c
Marta Ribeiro <63553878+martamribeiro@users.noreply.github.com>
2024-02-29 01:32:01 +0000

    Added game modes buttons. Added sound effects to buttons.

---
+++ RockPaperScissors/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Sprite[] gestures;
+
+    public TMP_Text player1, player2;
+
+    public void StartOnePlayerGame()
+    {
+        //changes in one player game
+        player2.text = "Player 2";
+        StartGame();
+    }
+
+    public void StartTwoPlayersGame()
+    {
+        //changes in two players game
+        player2.text = "Computer";
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        //common to 1 player and 2 players game modes
+    }
+
+    
+
+}
